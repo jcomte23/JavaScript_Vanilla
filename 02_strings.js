92e9f489ae7ba7f0d30f3409a105fcded8077903
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-12-24 16:18:43 -0500

    mejoras en strings

--- 02_strings.js
+++ 02_strings.js
@@ -1,5 +1,3 @@
-console.log("%cInicio del programa", "background-color: #843DFF; color: white; border-radius: 50px; font-weight: bold; padding-top: 5px; padding-right: 8px; padding-bottom: 5px; padding-left: 8px;")
-
 // ######################
 // DECLARACION DE STRINGS
 // ######################
