d75ac04489e807eeba50dd9e7cf13af65c6fff37
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-12-25 15:16:11 -0500

    mejoras en operadores

--- 04_operadores.js
+++ 04_operadores.js
@@ -1,4 +1,23 @@
 // ######################
+// TIPOS DE DATOS EN JS
+// ######################
+
+const var1 = 2
+const var2 = "2"
+const var3 = "hola mundo"
+const var4 = true
+const var5 = null
+const var6 = undefined
+
+console.log(typeof var1) // number
+console.log(typeof var2) // string
+console.log(typeof var3) // string
+console.log(typeof var4) // boolean
+console.log(typeof var5) // object
+console.log(typeof var6) // undefined
+
+
+// ######################
 // OPERADORES ARITMETICOS
 // ######################
 
@@ -22,6 +41,27 @@ console.log(13 % 5) // 3
 
 // Exponenciacion de 2 numeros
 console.log(2 ** 8) // 256
+
+// Incrementos de a una unidad
+let numeroTemp1 = 2
+numeroTemp1++
+console.log(numeroTemp1) // 3
+
+// Decrementos de a una unidad
+let numeroTemp2 = 5
+numeroTemp2--
+console.log(numeroTemp2) // 4
+
+// Incrementos de multiples unidades
+let numeroTemp3 = 3
+numeroTemp3 += 5
+console.log(numeroTemp3) // 8
+
+// Decrementos de multiples unidades
+let numeroTemp4 = 13
+numeroTemp4 -= 4
+console.log(numeroTemp4) // 9
+
 
 // #########################
 // OPERADORES DE COMPARACION
@@ -51,45 +91,44 @@ const PASSSWORD_CONFIRMATION = "ABC123"
 
 console.log(PASSSWORD !== PASSSWORD_CONFIRMATION) // true
 
+// Operador (mayor que)
+console.log(`¿${NUMERO1} es mayor que ${NUMERO3}? => ${NUMERO1 > NUMERO3}`)
+console.log(`¿${NUMERO3} es mayor que ${NUMERO2}? => ${NUMERO3 > NUMERO2}`)
+
+// Operador (menor que)
+console.log(`¿${NUMERO1} es menor que ${NUMERO3}? => ${NUMERO1 < NUMERO3}`)
+
+// Operador >= o <=
+console.log(`¿${NUMERO1} es mayor o igual que ${NUMERO2}? => ${NUMERO1 >= NUMERO2}`)
+console.log(`¿${NUMERO1} es menor o igual que ${NUMERO3}? => ${NUMERO1 <= NUMERO3}`)
+
+
 // #####################
 // OPERADORES DE LOGICOS
 // #####################
 
-//Operador (mayor que)
-console.log(`¿${NUMERO1} es mayor que ${NUMERO3}? => ${NUMERO1 > NUMERO3}`)
-console.log(`¿${NUMERO3} es mayor que ${NUMERO2}? => ${NUMERO3 > NUMERO2}`)
-
-//Operador (menor que)
-console.log(`¿${NUMERO1} es menor que ${NUMERO3}? => ${NUMERO1 < NUMERO3}`)
-
-//Operador >= o <=
-console.log(`¿${NUMERO1} es mayor o igual que ${NUMERO2}? => ${NUMERO1 >= NUMERO2}`)
-console.log(`¿${NUMERO1} es menor o igual que ${NUMERO3}? => ${NUMERO1 <= NUMERO3}`)
-
-//Null y undefined
-let varNull = null
-let varUndefined
-console.log(typeof varNull)
-console.log(typeof varUndefined)
-console.log(varNull == varUndefined)
-console.log(varNull === varUndefined)
-
-// > o < de forma estricta
+// Operador AND (&&) 
 let estatura = "1.96"
 console.log(estatura >= 1.71 && typeof estatura == "string")
 console.log(estatura >= 1.71 && typeof estatura == "number")
 
-// Operador OR || 
+// Operador OR (||) 
 console.log(estatura >= 1.71 || typeof estatura == "number")
 
-// #######################
-// OPERADORES DE TERNARIOS
-// #######################
-;(estatura <= 1.65)
-    ? console.log("Es una persona bajita")
-    : (estatura >= 1.66 && estatura <= 1.75)
-        ? console.log("Es una persona dentro del promedio")
-        : console.log("Es una persona alta")
+// Operador NOT logico (!) 
+let variable = true
+console.log(variable) // true
+console.log(!variable) // false
+
+
+    // #######################
+    // OPERADORES DE TERNARIOS
+    // #######################
+    ; (estatura <= 1.65)
+        ? console.log("Es una persona bajita")
+        : (estatura >= 1.66 && estatura <= 1.75)
+            ? console.log("Es una persona dentro del promedio")
+            : console.log("Es una persona alta")
 if (estatura <= 1.65) {
     console.log("Es una persona bajita")
 } else {
